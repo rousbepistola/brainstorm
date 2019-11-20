efbe1284fd8f719a16ba7f9d2970d02443565d2a
rousbepistola <56054397+rousbepistola@users.noreply.github.com>
2019-11-20 16:15:58 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <title>Brainstrom Live</title>
+        <meta name="description" content="An intelligent way to collaborate with other brainstorm students! Grow and learn together with Brainstorm Live!">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+        <!-- ESSENTIALS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+        <!-- font-awesome -->
+        <script src="https://kit.fontawesome.com/3d3ae9b37d.js" crossorigin="anonymous"></script>
+    
+        <!-- google font -->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat+Alternates&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Bad+Script&display=swap" rel="stylesheet">
+    
+        <!-- bootstrap  -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+        </script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+        </script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+        </script>
+    
+        <!-- cdn for div animation michalsnik.github.io/aos/ -->
+        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+        <!-- stylesheet personal-->
+        <link rel="stylesheet" href="./stylesheet.css">
+    </head>
+
+    
+    <body>
+
+    <!-- navigation -->
+        <div class="nav" style="z-index:5">
+            <input type="checkbox" id="nav-check">
+            <div class="nav-header">
+                <div class="nav-title">
+                Brainstorm Live
+                </div>
+            </div>
+            <div class="nav-btn">
+                <label for="nav-check">
+                <span></span>
+                <span></span>
+                <span></span>
+                </label>
+            </div>
+            
+            <div class="nav-links">
+                <a href="//github.io/jo_geek" target="_blank">Home</a>
+                <a href="http://stackoverflow.com/users/4084003/" target="_blank">Signup</a>
+                <a href="https://in.linkedin.com/in/jonesvinothjoseph" target="_blank">Brainstorm</a>
+            </div>
+        </div>
+    
+    <!-- banner -->
+        <div  class="col md 12" id="bannerHome"></div>  
+
+
+    <!-- school logo -->
+        <div class="container">
+            <div class="row" style="text-align:center; padding:0.5rem; margin:1.5rem;   font-family: 'montserrat alternative';">
+                <div class="col-md-2"></div>
+                <div class="col-md-8" style="font-size:1.5rem;">Trusted more than 300 schools across North America</div>
+                <div class="col-md-2"></div>
+            </div>
+
+            <div class="row" style="text-align:center; padding:0.5rem; margin:1.5rem;   font-family: 'montserrat alternative';">
+                <div class="col-md-2"></div>
+                <div class="col-md-8" >
+                    <img style="height:10rem; width:auto; border-radius:50%; margin:0.5rem 2rem;" src="./media/school1.png" alt="school logo 1">
+                    <img style="height:10rem; width:auto; border-radius:50%; margin:0.5rem 2rem;" src="./media/school3.png" alt="school logo 3">
+                    <img style="height:10rem; width:auto; border-radius:50%; margin:0.5rem 2rem;" src="./media/school2.png" alt="school logo 2">
+                </div>
+                <div class="col-md-2"></div>
+            </div>
+
+        </div>
+
+    <!-- home page information middle -->
+        <div class="container" >
+            <div class="row" >
+                <div class="col-md-1"></div>
+
+                <div class="col-md-5" style="text-align:justify; margin:1.5rem">
+                    <h3>Better Learning Engagement</h3>
+                    <p style="line-height:3rem; text-align:justify">
+                        &nbsp;&nbsp;&nbsp;Some things can be hard to say out loud while learning. If you need a little help and one-on-one 
+                        time with someone or a community who gets how to compute for something, you can connect directly with brainstorm students
+                        through Live Chat.
+                    </p>
+
+                </div>
+
+                
+                <div class="col-md-5"></div>
+
+                <div class="col-md-1"></div>
+            </div>
+        </div>
+
+
+
+
+        
+            
+            
+
+
+        <div style="height: 100vh"></div>
+        
+        
+        <script src="./main.js" async defer></script>
+    </body>
+</html>
+
+
+
+<!-- you can do this you bastard! -->
